a1bddcd8faf8c75970e218775f6fbbba89c02819
mdsaharshital <mdsaharshitol@gmail.com>
2022-12-27 02:31:17 +0600

    store created with login and signup fuction done on firebase and ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
-
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import auth from "./firebase.config";
+import { useDispatch } from "react-redux";
+import { getUser } from "./features/auth/authSlice";
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(getUser(user.email));
+      }
+    });
+  }, [dispatch]);
   return (
     <>
       <RouterProvider router={routes} />
---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,82 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import auth from "./../../firebase.config";
+const initialState = {
+  role: "",
+  email: "",
+  isLoading: true,
+  isError: false,
+  error: "",
+};
+export const createUser = createAsyncThunk(
+  "auth/createUser",
+  async ({ email, password }) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+  }
+);
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async ({ email, password }) => {
+    const data = await signInWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.email = "";
+    },
+    getUser: (state, { payload }) => {
+      state.email = payload;
+      state.isLoading = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.email = "";
+        state.isLoading = true;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(createUser.fulfilled, (state, { payload }) => {
+        state.email = payload;
+        state.isLoading = false;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.email = "";
+        state.isLoading = false;
+        state.isError = true;
+        state.error = action.error.message;
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.email = "";
+        state.isLoading = true;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(loginUser.fulfilled, (state, { payload }) => {
+        state.email = payload;
+        state.isLoading = false;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.email = "";
+        state.isLoading = false;
+        state.isError = true;
+        state.error = action.error.message;
+      });
+  },
+});
+export const { logout, getUser } = authSlice.actions;
+export default authSlice.reducer;
